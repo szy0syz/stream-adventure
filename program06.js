137cb5234f660d0a2a6e93459227f2084215e051
szy0syz <szy0syz@gmail.com>
2016-06-05 15:24:26 +0800

    lesson06 OK

---
+++ program06.js
@@ -0,0 +1,23 @@
+// var http = require('http')
+// var concat = require('concat-stream')
+
+// var server = http.createServer(function(req,res){
+// 	if (req.method === "GET") {
+// 		req.pipe(concat(function(body){
+// 			var obj = JSON.parse(body);
+// 			res.end(Object.keys(obj).join('\n'));
+// 		}))
+// 	}
+// 	else{
+// 		res.end();
+// 	}
+// })
+// server.listen(5000)
+var concat = require('concat-stream');
+
+process.stdin.pipe(concat(function(body){
+	console.log(body.reverse().toString())
+	// var obj = JSON.parse(body);
+	// console.log('obj_:' + obj)
+	//this.push(Object.keys(obj).reverse().join('\n'));
+}))
