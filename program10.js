793108c5985606988de73aa392cc56ea291ae3cc
szy0syz <szy0syz@gmail.com>
2016-06-06 21:40:20 +0800

    lesson10 OK

--- program10.js
+++ program10.js
@@ -1,15 +1,32 @@
 var trumpet = require('trumpet');
 var through = require('through2');
-
+ 
 var tru = trumpet();
-
+ 
 var tr = through(function(buf,_,next){
-	this.push(buf.toString().toUpperCase());
-	next();
+    this.push(buf.toString().toUpperCase()+ '</span>');
+    next();
 })
-
+ 
 process.stdin.pipe(tru);
+ 
+var writeStream = tru.select('.loud').createWriteStream();
+var readStram = tru.select('.loud').createReadStream();
+ 
+readStram.pipe(tr).pipe(writeStream);
+ 
+tru.pipe(process.stdout);
 
-var stream = tru.select('.loud').createStream();
 
-stream.pipe(tr).pipe(process.stdout);
+
+  // var trumpet = require('trumpet');
+  // var through = require('through2');
+  // var tr = trumpet();
+ 
+  // var loud = tr.select('.loud').createStream();
+  // loud.pipe(through(function (buf, _, next) {
+  //     this.push(buf.toString().toUpperCase());
+  //     next();
+  // })).pipe(loud);
+ 
+  // process.stdin.pipe(tr).pipe(process.stdout);
